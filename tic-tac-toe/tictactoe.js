9faa3f735ce168fe8dca6ae7d117c4be1801ae4e
Gaël Le Bon <gael.m.lebon@gmail.com>
2016-08-31 12:34:07 +0200

    Animations ! Game Finally Ends ! Reset Button !

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -1,50 +1,69 @@
 //Initialize board
 var boardSize = 3;
 var board = [];
+var imgOpath = 'img/mlp_fluttershy.png';
+var imgXpath = 'img/mlp-rdash.jpg';
+var symbole = '';
+var finished = false;
+var tour = 0;
+
+function reset(){
 for(var i = 0; i < boardSize; i++){
 	board[i] = [];
 }
 
-var symbole = '';
+finished = false;
+tour = 0;
+symbole = '';
 
-var tour = 0;
+
+
+
+}
 var gid = function(divId){
 	return document.getElementById(divId);
 }
 
-for(var i = 0; i < boardSize; i++){
-	for(var j = 0; j < boardSize; j++){
-		var divName = 'c'+i.toString()+j.toString();
-
-		gid(divName).onclick = function(i,j){
-			return function(){
-				
-				jouer(i,j);
-			};
-		}(i,j)
-	}
-}
-
-
 function jouer(i,j){
-	spot = gid('c'+i+j)
-	spot.onclick = null;
-	symbole = tour%2 ? 'O' : 'X';
-	tour++;
-	board[i][j] = symbole;
-	spot.innerHTML = symbole;
-	if(tour > 4){
-		var end = isFinished(i,j);
-		if(end){
-			gid('winner').innerHTML = symbole +' Win !';
+	if(!finished){
+		spot = gid('c'+i+j);	
+		symbole = tour%2 ? 'O' : 'X';
+		tour++;
+		board[i][j] = symbole;
+		image = document.createElement('img');
+		image.className += 'img-responsive img-round animated rollIn';
+		console.log(symbole);
+		if(symbole == 'O'){
+			image.src = imgOpath;
 		}
-		if(tour >= (boardSize * boardSize) && !end){
-			gid('winner').innerHTML = 'Draw !';
+		else{
+			image.src = imgXpath;
 		}
+		spot.appendChild(image);
+		if(tour > 4){
+			var end = hasWon(i,j);
+			if(end){
+				finished = true;
+				var winStr = '';
+				if(symbole == 'O'){
+					winStr = 'Fluttershy Wins !'
+				}
+				else{
+					winStr = 'Rainbow Dash wins !'
+				}
+				gid('winner').innerHTML = winStr;
+			}
+			if(tour >= (boardSize * boardSize) && !end){
+				finished = true;
+				gid('winner').innerHTML = 'Draw !';
+			}
+		}
+		spot.onclick = null;
 	}
+	
 }
 
-function isFinished(i,j){
+function hasWon(i,j){
 	// Check Columns
 	for(var y = 0; y < boardSize; y++){
 		if(board[y][j] != symbole){
@@ -90,5 +109,19 @@ function isFinished(i,j){
 				//win for symbole
 			}
 		}
+	}
+}
+
+reset();
+for(var i = 0; i < boardSize; i++){
+	for(var j = 0; j < boardSize; j++){
+		var divName = 'c'+i.toString()+j.toString();
+
+		gid(divName).onclick = function(i,j){
+			return function(){
+				
+				jouer(i,j);
+			};
+		}(i,j)
 	}
 }
